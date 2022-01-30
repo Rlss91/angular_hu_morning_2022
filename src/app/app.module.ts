410edef38675fdcfdc7ac8551e12f9bf6cfc4ce6
rlss91 <rlss91@gmail.com>
2022-01-30 10:44:15 +0200

    calculator 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { ClassDirectiveComponent } from './components/directives/attribute/class
 import { StyleDirectiveComponent } from './components/directives/attribute/style-directive/style-directive.component';
 import { NgifDirectiveComponent } from './components/directives/structural/ngif-directive/ngif-directive.component';
 import { NgforDirectiveComponent } from './components/directives/structural/ngfor-directive/ngfor-directive.component';
+import { Calculator2Component } from './components/calculator2/calculator2.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, CalculatorComponent, ClassDirectiveComponent, StyleDirectiveComponent, NgifDirectiveComponent, NgforDirectiveComponent],
+  declarations: [AppComponent, NavbarComponent, CalculatorComponent, ClassDirectiveComponent, StyleDirectiveComponent, NgifDirectiveComponent, NgforDirectiveComponent, Calculator2Component],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/calculator2/calculator2.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator2',
+  templateUrl: './calculator2.component.html',
+  styleUrls: ['./calculator2.component.css'],
+})
+export class Calculator2Component implements OnInit {
+  num1: number;
+  num2: number;
+  result: number;
+  operator: string;
+  line: string;
+  numbersArr: number[];
+  operatorsArr: string[];
+  constructor() {
+    this.num1 = NaN;
+    this.num2 = NaN;
+    this.result = NaN;
+    this.operator = '';
+    this.line = '';
+    this.numbersArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    this.operatorsArr = ['+', '-', '*', '/'];
+  }
+
+  ngOnInit(): void {}
+  //15+10=?
+  handleNumberButtonClick(number: number): void {
+    if (this.operator == '') {
+      if (isNaN(this.num1) || this.num1 === 0) {
+        this.num1 = number;
+      } else {
+        this.num1 = this.num1 * 10 + number;
+      }
+    } else {
+      if (isNaN(this.num2) || this.num2 === 0) {
+        this.num2 = number;
+      } else {
+        this.num2 = this.num2 * 10 + number;
+      }
+    }
+    this.createLine();
+  }
+  handleOperationButtonClick(op: string): void {
+    this.operator = op;
+    this.createLine();
+  }
+
+  handleCalcButtonClick(): void {
+    switch (this.operator) {
+      case '+':
+        this.result = this.num1 + this.num2;
+        break;
+      case '-':
+        this.result = this.num1 - this.num2;
+        break;
+      case '*':
+        this.result = this.num1 * this.num2;
+        break;
+      case '/':
+        this.result = this.num1 / this.num2;
+        break;
+    }
+    this.num1 = NaN;
+    this.num2 = NaN;
+    this.operator = '';
+    this.line = this.result + '';
+  }
+
+  createLine(): void {
+    this.line = '';
+    if (!isNaN(this.num1)) {
+      this.line += this.num1;
+    }
+    if (this.operator != '') {
+      this.line += this.operator;
+    }
+    if (!isNaN(this.num2)) {
+      this.line += this.num2;
+    }
+  }
+}
